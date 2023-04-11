9eb110f8513d0e5283e8ca3db6444bf9b9077428
Dmsooja <dev@sooja.fr>
2023-04-11 12:29:42 +0200

    Tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./slices/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue" : "#0F172A",
+        "vibrant-blue" : "#2563EB",
+        "black" : "0F172A",
+        "light-black" : "#334155",
+        "light-blue" : "60A5FA",
+      }
+    },
+  },
+  plugins: [],
+}
+
