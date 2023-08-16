4c73d836da4b58bd78ed1a49f7f24e06684a588b
Dmsooja <dev@sooja.fr>
2023-08-16 16:55:45 +0200

    update SM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { PrismicProvider } from "@prismicio/react";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { repositoryName } from "@/prismicio";
-import "highlight.js/styles/github-dark.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
