fdc0790706d7dfa392ef38ddc7ed9ac7834e6725
Dmsooja <dev@sooja.fr>
2023-09-25 13:12:21 +0200

    Implement personnalisation

--- middleware.ts
+++ middleware.ts
@@ -2,49 +2,55 @@ import { NextRequest, NextResponse } from "next/server";
 import { GrowthBook } from '@growthbook/growthbook'
 
 
-const COOKIE = 'visitor_profile';
+export async function middleware(request: NextRequest, response: NextResponse) {
 
-
-
-export async function middleware(request: NextRequest) {
-
-    // We only want to run the A/B test on the homepage
     const pathname = request.nextUrl.pathname;
-    if (pathname !== "/lp/get-certified") {
-        return NextResponse.next()
-    }
-    console.log({ pathname })
 
-    // Get existing visitor cookie or create a new one
-    let visitor_profile = request.cookies.get(COOKIE);
+    const cookie = request.cookies.get("visitor_profile")
 
-    // Check the request URL or any other criteria to determine if it's a marketing or developer article
-    const isMarketingArticle = request.url.includes('/blog/marketing');
-    const isDeveloperArticle = request.url.includes('/blog/development');
+    const profile = request.cookies.get("visitor_profile")?.value
 
-    // return NextResponse.next()
+    const res = NextResponse.next();
 
-    const response = NextResponse.next()
-
-    // Set the cookies
-    if (isMarketingArticle) {
-        // Set a marketing cookie
-        response.cookies.set(COOKIE, 'marketer');
-        console.log(JSON.stringify(request.cookies.get('visitor_profile')?.value))
-    } else if (isDeveloperArticle) {
-        // Set a developer cookie
-        response.cookies.set(COOKIE, 'developer');
-        console.log(JSON.stringify(COOKIE))
+    // Set marketer profile with cookie
+    if (request.url.includes('/blog/marketing')) {
+        res.cookies.set('visitor_profile', 'marketer')
+        console.log("Cookie set as", cookie)
+        return res
     }
 
-    // Redirect to the matching variant of the landing page depending on the visitor's profile
+    // Set developer profile with cookie
+    if (request.url.includes('/blog/development')) {
+        res.cookies.set('visitor_profile', 'developer')
+        console.log("Cookie set as", cookie)
+        return res
+    }
 
-    // return NextResponse.redirect(new URL("/", request.url))
+    // Delete profile when accessing homepage
+    if (pathname == ('/')) {
+        res.cookies.delete('visitor_profile')
+        console.log("Profile deleted", cookie)
+        return res
+    }
+
+    // Display the matching variant of the landing page depending on the visitor's profile
+    // Rewrite the variant path so it displays the original page URL slug
+
+    if (pathname == '/lp/get-certified') {
+        console.log("Your profile is", profile)
+        if (profile == 'marketer') {
+            return NextResponse.rewrite(new URL('/lp/get-certified-marketers', request.url))
+        }
+        if (profile == 'developer') {
+            return NextResponse.rewrite(new URL('/lp/get-certified-developers', request.url))
+        }
+        
+    }
 
 
 }
 
 // Match to these pages
 export const config = {
-    matcher: ['/lp/get-certified', '/blog/marketing/(.*)', '/blog/development/(.*)']
+    matcher: ['/', '/lp/get-certified', '/blog/marketing/(.*)', '/blog/development/(.*)']
 }
