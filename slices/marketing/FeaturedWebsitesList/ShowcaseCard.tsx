a8831c2860a92f7f3a536ec516af219fa349d8cb
Dmsooja <dev@sooja.fr>
2025-01-08 12:00:29 +0100

    conditional rendering

--- slices/marketing/FeaturedWebsitesList/ShowcaseCard.tsx
+++ slices/marketing/FeaturedWebsitesList/ShowcaseCard.tsx
@@ -10,12 +10,14 @@ export const ShowcaseCard = ({ website }: { website: Website }) => {
       <div className="relative">
         <div className="absolute inset-0 rounded-xl shadow-website-screenshot" />
         <div className="aspect-w-16 aspect-h-12 rounded-lg overflow-hidden relative bg-gray-F7">
-          <img
-            className="opacity-100 scale-100 transition-all ease-in-out duration-500 object-cover"
-            src={website?.screenshot.url}
-            alt={website?.screenshot.alt}
-            sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, 33vw"
-          />
+          {website.screenshot && (
+            <img
+              className="opacity-100 scale-100 transition-all ease-in-out duration-500 object-cover"
+              src={website?.screenshot.url}
+              alt={website?.screenshot.alt}
+              sizes="(max-width: 639px) 100vw, (max-width: 1023px) 50vw, 33vw"
+            />
+          )}
         </div>
       </div>
       <footer className="flex items-center justify-between text-base font-headings text-gray-900 mt-4 px-2">
